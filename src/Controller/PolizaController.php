a42ae378c720e045d331c0063f6652906baaabd9
Jose Manuel Rosado <nepenteh@gmail.com>
2023-01-30 10:13:18 +0100

    Modificaciones después de publicar

--- src/Controller/PolizaController.php
+++ src/Controller/PolizaController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/poliza')]
+# [Route('/')]
 class PolizaController extends AbstractController
 {
     #[Route('/', name: 'app_poliza_index', methods: ['GET'])]
